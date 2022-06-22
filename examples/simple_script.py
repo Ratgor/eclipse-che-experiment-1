d776c55c3651be6b2ca6c77fa57ec9dad7a3f06c
Ratgor <ratgorfromrassvetera@gmail.com>
2022-06-22 08:26:59 +0300

    Update simple_script.py

--- examples/simple_script.py
+++ examples/simple_script.py
@@ -2,4 +2,8 @@ gr = f"Hello from Eclipse Che VS Code IDE"
 print(gr)
 
 import os, sys
-print(f"currnet working directory {os.getcwd()} with content {os.listdir()}")
+print(f"currnet working directory {os.getcwd()} with the content {os.listdir()}")
+
+print(f"system environment variables:")
+for k,v in os.environ.items():
+    print(f"{k} == {v}\n") 
